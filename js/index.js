2c2a78d1c557f5c818219eaeda29ab1f5e9f8179
Anthony Ponson <antonyponson@gmail.con>
2022-02-01 21:49:18 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,176 @@
+////opacity////
+
+
+$(document).ready(function(){
+    
+    $(window).scroll(function(){
+        
+    
+        if($(this).scrollTop() > 0){
+            
+            $(" .home-content-box ").css( { "opacity" : "0.5"})
+            
+        }
+        
+        else{
+            $(" .home-content-box").css({"opacity" : "1"})
+        }
+        
+        
+    })
+    
+});
+
+
+$(function(){
+   
+    $("#leo-portfolio").owlCarousel({
+        
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+//        loop: true,
+    });
+    
+});
+
+
+//contact form    
+
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input2').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+            
+  
+    
+    /*==================================================================
+    [ Validate ]*/
+    var name = $('.validate-input input[name="name"]');
+    var email = $('.validate-input input[name="email"]');
+    var message = $('.validate-input textarea[name="message"]');
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(name).val().trim() == ''){
+            showValidate(name);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(message).val().trim() == ''){
+            showValidate(message);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input2').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*===================================================================================================================================================================================================================================================================================================================================*/
+
+/* ========================================================== */
+	/*   Navigation Background Color                              */
+	/* ========================================================== */
+	
+	$(window).on('scroll', function() {
+		if($(this).scrollTop() > 450) {
+			$('.navbar-fixed-top').addClass('opaque');
+		} else {
+			$('.navbar-fixed-top').removeClass('opaque');
+		}
+	});
+ 
+	
+	/* ========================================================== */
+	/*   Hide Responsive Navigation On-Click                      */
+	/* ========================================================== */
+	
+	  $(".navbar-nav li a").on('click', function(event) {
+	    $(".navbar-collapse").collapse('hide');
+	  });
+
+	
+	/* ========================================================== */
+	/*   Navigation Color                                         */
+	/* ========================================================== */
+	
+	$('#navbarCollapse').onePageNav({
+		filter: ':not(.external)'
+	});
+
+
+	/* ========================================================== */
+	/*   SmoothScroll                                             */
+	/* ========================================================== */
+	
+	$(".navbar-nav li a, a.scrool").on('click', function(e) {
+		
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+		
+		$('html,body').animate({scrollTop:target_top -70}, 1000);
+			return false;
+		
+	});
+
+
+
+
